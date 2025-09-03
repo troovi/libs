6e5a7c31481ff1c985bdaff92d01d0c735b76620
Pavel <troovi@icloud.com>
2025-09-03 20:49:26 +0800

    update

--- packages/uikit/src/Popup/index.tsx
+++ packages/uikit/src/Popup/index.tsx
@@ -1,6 +1,9 @@
 import './Popup.scss'
 import cn from 'classnames'
 import { Overlay, OverlayProps } from '../Overlay'
+import { Cross } from '@blueprintjs/icons'
+import { box } from '@troovi/utils-browser'
+import { Scrollable } from '..'
 
 interface PopupProps extends OverlayProps {
   children: React.ReactNode
@@ -14,5 +17,28 @@ export const Popup = ({ children, className, ...overlayProps }: PopupProps) => {
         <div className={cn('popup', className)}>{children}</div>
       </div>
     </Overlay>
+  )
+}
+
+Popup.Header = ({ title, close }: { title: string; close: () => void }) => {
+  return (
+    <div className="popup-header flex items-center justify-between">
+      <div className="text-lg font-semibold">{title}</div>
+      <div className="close-button center" onClick={close} style={box(28)}>
+        <Cross size={20} />
+      </div>
+    </div>
+  )
+}
+
+Popup.Footer = ({ children }: { children: React.ReactNode }) => {
+  return <div className="popup-footer ">{children}</div>
+}
+
+Popup.ScrollContainer = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <Scrollable implementation="inner" scrollY padding={16} thumbPadding={6} thumbColor="#ffffff26">
+      {children}
+    </Scrollable>
   )
 }
