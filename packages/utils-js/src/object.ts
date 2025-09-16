d0255bf9abc80c48c4896b650bd0e664e9088ec9
Pavel <troovi@icloud.com>
2025-09-16 23:41:53 +0800

    update

--- packages/utils-js/src/object.ts
+++ packages/utils-js/src/object.ts
@@ -1,8 +1,20 @@
+export const isEmpthy = (object: object) => {
+  return Object.keys(object).length === 0
+}
+
 export const omit = <T extends object>(object: T, key: keyof T) => {
   delete object[key]
   return object
 }
 
-export const isEmpthy = (object: object) => {
-  return Object.keys(object).length === 0
+export const partial = <T extends object>(object: T): T => {
+  const response = {} as T
+
+  for (const key in object) {
+    if (object[key]) {
+      response[key] = object[key]
+    }
+  }
+
+  return response
 }
