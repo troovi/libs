1f8c0620df53080419190302cb5e78efe044eaa7
Pavel <troovi@icloud.com>
2026-02-03 20:03:50 +0800

    update

--- packages/uikit/src/LoadingButton/index.tsx
+++ packages/uikit/src/LoadingButton/index.tsx
@@ -3,14 +3,19 @@ import { UseLoadingProps, useLoading } from '../__hooks/use-loading'
 
 export interface LoadingButtonProps extends UseLoadingProps, Omit<ButtonProps, 'onClick'> {}
 
-export const LoadingButton = ({ onClick, appearance = 'primary', ...rest }: LoadingButtonProps) => {
+export const LoadingButton = ({
+  onClick,
+  appearance = 'primary',
+  loading,
+  ...rest
+}: LoadingButtonProps) => {
   const { isLoading, isError, handleClick } = useLoading({ onClick })
 
   return (
     <Button
       appearance={isError ? 'negative' : appearance}
       {...rest}
-      loading={isLoading}
+      loading={isLoading || loading}
       onClick={handleClick}
     />
   )
