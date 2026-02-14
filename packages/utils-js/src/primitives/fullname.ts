a23353fb6cdb0f6abe5770a351f9d80ee7d33e80
Pavel <troovi@icloud.com>
2026-02-14 23:30:57 +0800

    update

--- packages/utils-js/src/primitives/fullname.ts
+++ packages/utils-js/src/primitives/fullname.ts
@@ -10,8 +10,21 @@ export const isNamesNotEqual = (A: FullName, B: FullName) => {
   return A.name !== B.name || A.surname !== B.surname || A.patronymic !== B.patronymic
 }
 
-export const getInitials = ({ name, patronymic }: Omit<FullName, 'surname'>, joinChart = '') => {
-  return [capitalize(name[0]), capitalize(patronymic[0])].join(joinChart)
+export const getInitials = (
+  { name = '', patronymic = '' }: Omit<FullName, 'surname'>,
+  joinChart = ''
+) => {
+  const chunks: string[] = []
+
+  if (name[0]) {
+    chunks.push(capitalize(name[0]))
+  }
+
+  if (patronymic[0]) {
+    chunks.push(capitalize(patronymic[0]))
+  }
+
+  return chunks.join(joinChart)
 }
 
 export const getShortName = ({ name, surname, patronymic }: FullName) => {
